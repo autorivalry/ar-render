f0c41bb7f380d91d623fe01a8899e4cf06153e72
Jordan Skole <jordan@usebenchmark.com>
2015-12-28 21:17:21 -0500

    fetch matchup from fb

--- routes/matchup.js
+++ routes/matchup.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require("firebase");
+var matchupsRef = new Firebase('https://auto-rivalry.firebaseio.com/matchups');
 
 /* GET matchups listing. */
 router.get('/:id', function(req, res) {
-  res.render('index', {title: 'Matchup - ' + req.params.id });
-  // res.send('respond with a resource/view id: ' + req.params.id);
+
+  // get the matchup from the server
+  var matchupRef = matchupsRef.child(req.params.id);
+  matchupRef.once('value', function (snapshot) {
+    res.render('index', snapshot.val());
+  });
+  
 });
 
 module.exports = router;
